b8a2ae903d7e1ddc83cc43fc371f473519485a9e
zakariyaxaaji <zakariyaxaaji@gmail.com>
2024-10-15 21:02:03 +0200

    working on the overlay, problems keep poping up 😣

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       <Container>
         <ProductList />
-        <Cart /> {/* cart here is not using any props */}
+        <Cart /> 
         <Overlay/>
       </Container>
     </div>
